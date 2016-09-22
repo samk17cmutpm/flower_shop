b463e468392cfb8074fdc925298866247affde1a
samk17cmutpm <samk17cmutpm@gmail.com>
2016-09-22 09:23:15 +0700

    Các panel, ko nên bo tròn 4 đầu nghe, và ko nên có các bóng mờ nghe

--- app/src/main/java/khoaluan/vn/flowershop/Base.java
+++ app/src/main/java/khoaluan/vn/flowershop/Base.java
@@ -12,7 +12,7 @@ public interface Base {
     int TAB_INFO = 4;
 
     int GRID_VIEW_SIZE = 2;
-    int PRODUCT_DISTANCE = 5;
+    int PRODUCT_DISTANCE = 1;
     int ADVERTISING_DISTANCE = 2;
 
     int RECYCLER_VIEW_DISTANCE = 1;
